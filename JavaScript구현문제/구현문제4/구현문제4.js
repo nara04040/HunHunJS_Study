add9896a0f63e79cc6e466f94c09d70eedda25d3
nara04040 <nara04040@naver.com>
2023-01-25 01:02:20 +0900

    구현문제 4,5수정

--- JavaScript구현문제/구현문제4/구현문제4.js
+++ JavaScript구현문제/구현문제4/구현문제4.js
@@ -5,6 +5,5 @@ const navHeader = document.querySelector(".nav-header");
 navHeader.addEventListener("click", () => {});
 
 navToggle.addEventListener("click", () => {
-  //   links.style.display = "block";
   links.classList.toggle("show-links");
 });
